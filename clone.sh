1e49596be729d9f3f01b901d9c6aeb21ef098b97
kaylor.chen <kaylor.chen@qq.com>
2024-12-19 09:36:44 +0800

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # apt update
-git clone https://github.com/airockchip/rknn-toolkit2.git --depth 1 tmp
+git clone https://github.com/orangepi-xunlong/linux-orangepi.git tmp
 tar -czvf git.tar.gz tmp/.git
