4e2150a969b63846a886e85048da623c68613390
kaylor.chen <kaylor.chen@qq.com>
2026-06-04 21:31:39 +0800

    Change git clone command to new repository and branch

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # apt update
-git clone https://github.com/kaylorchen/rknn-toolkit2.git --depth 10 tmp
+git clone -b ubuntu/rz-v2h-rdk https://github.com/Renesas-SST/linux-rz.git --single-branch --depth 1 tmp
 tar -czvf git.tar.gz tmp/.git
