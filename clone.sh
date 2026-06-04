e2924c8151c23e42dcd38c22b9c58bb3018dcd27
kaylor.chen <kaylor.chen@qq.com>
2026-06-04 21:32:52 +0800

    Change git clone repository to rz-utils

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # apt update
-git clone -b ubuntu/rz-v2h-rdk https://github.com/Renesas-SST/linux-rz.git --single-branch --depth 1 tmp
+git clone -b ubuntu/rz-v2h-rdk https://github.com/Renesas-SST/rz-utils.git --single-branch --depth 1 tmp
 tar -czvf git.tar.gz tmp/.git
